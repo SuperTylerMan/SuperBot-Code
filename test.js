7b6783ff82d42537f303cc9c767a334eae09a22a
SuperTylerMan <67520465+SuperTylerMan@users.noreply.github.com>
2020-07-26 08:19:16 -0400

    Version 1.0.0
    Added some // texts in there.
    
    More updates coming soon...

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var DiscordUserID = require("./Data/name.json");
+var DiscordUserID = require("./Data/name.json"); //Must Come from that file, or else, the JSON file will not work.
 
-let user = DiscordUserID.find(item => item.DiscordUserID === "value");
-console.log(DiscordUserID)
+let user = DiscordUserID.find(item => item.DiscordUserID === "value"); //Value code isn't set up yet fully. 
+console.log(DiscordUserID) //In terminal, I will be able to see the values of the DiscordUserID
