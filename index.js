ec048442503c6aa8fe907fdc20bd346ee2bb84f7
SuperTylerMan <67520465+SuperTylerMan@users.noreply.github.com>
2020-10-01 13:34:37 -0400

    Version 1.13.0
    What's new?
    (Accidently leaked the bot token for the 23rd time this year. A new record for SuperTylerMan! 👍)
    -A ton of changes with the files. I know I have been very slow on Github here recently. It is because I have school, so please do not mind. 
    
    -basiccommand.js has bearly changed. We changed b/info versions and b/info about. Because there was a new node.js update, we had to update to the latest recommended version of node.
    -ectstuff.js had no changes. The code there is still exactly the same 
    -moderationcommands.js had the most changes out of any other file within the past few updates. There are some areas where you cannot use commands in direct messages anymore, new changes with b/suggest, and b/say had some changes too.
    
    Check out each file in "Bot Code Files" for the SuperBot on GitHub for the code.
    -More updates coming soon...

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 const Discord = require('discord.js'); //without discord.js, the bot really cannot run -__-
 const doenvv = require('dotenv')
 const bot = new Discord.Client();
-const token = ("NzI2NDc1OTMwNDMxNzgyOTky.Xvd1bw.e6NLdplljdvBHgg2-XMR7NGgtJM") //This token is important, and this is what runs the bot properly. Without it, the bot will not run.
+const token = ("Place Bot token here") //This token is important, and this is what runs the bot properly. Without it, the bot will not run.
 const PREFIX = ('b/' || 'sb!') //This prefix is b/. This is what the bot commands will respond to, when that prefix is used. Not even a joke, this bot can respond to this <letter><letter>. It is not even a joke
 var version = '1.1.0 (Stage Alpha)'; //This is the version of the bot. This is on top so I can change it anytime, without getting lost, and keep scrolling down, and down, and down...
 var help = 'This is being added soon...' //This is being added soon, when there is simply way to many commands, we will add this later on...
