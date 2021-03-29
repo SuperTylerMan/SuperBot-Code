c7cdd01b191c923b1d8e5dd4f125b37ace5648e2
SuperTylerMan <67520465+SuperTylerMan@users.noreply.github.com>
2021-03-28 20:12:08 -0400

    Version 1.3.2 Stage Alpha
    Probably one of my biggest updates I have ever done! Lets go through the entire list!
    
    basiccommands.js
    -New command was added! b/uptime - This shows the uptime in the bot (in minutes). 
    -b/ping has changed. Now it's a fully embedded command. 
    
    moderationcommands.js
    -Another new command! b/announcement. This makes your announcement command into an embedded announcement. I have not seen this on a ton of bots, so I added it so server owners or administrators can use this to create an announcement if they didn't want it in plain text
    -The command b/report has changed slightly. Now if the player isn't @ when you send a report, the report will not go through. If there is no #reports channel, the bot will send a message saying that report cannot be sent. Also, the user's # will now show when you send a report to the moderators. This should fix a lot of issues for staff members seeing reports that have been filled with chaos.
    
    ectstuff.js
    -New command! b/mcserverstatus - Show a Minecraft Java Edition server status on discord, without logging onto Minecraft! This was added as a nice new feature for any Minecraft Java players who wants to check their or someone else's server without having to log onto Minecraft java. (NPM package must be installed to use this)
    -Another new command! b/sayembed - Repeats a message into an embed. No coding, all in a nice embed form!
    -The command b/suggest has changed slightly. If there is no #suggestions channel, the bot will not send and will send a message saying that you cannot send that suggestion. Also, the # of the player's name now shows when you send a suggestion
    -b/poll has changed slightly. Now when you make a poll, your # of your username will now show up when a poll is created.
    -b/discordprofile has changed. Now it shows more information such as #, full discord name, and your ID in discord (which is public if you have developer mode on in settings to copy the ID).
    
    Note: Some parts of the code were changed when uploading files to support the GitHub files I upload here for it to work properly when downloading/testing. 
    
    To check out all the code, please go to the folder, "Bot Code Files" to see the code. 
    Leave a comment to suggest anything if you want. Thanks!

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-//This is all the basics to run the bot so it can run online. You can find all of this on the Github, when you downloaded it.
-//It starts on Line 3 and ends on line 8. Const = consistent which also means these things cannot change. 
-//There are some stuff on here like var (variable) that is used elsewhere, so that I can write it down there, without having to re-write the same thing over and over again
-//Here are the steps to use the discord bot
-//1) To start the bot, you will need to download discord.js, and node. Watch this video here to download it, and run it: https://www.youtube.com/watch?v=X_qg0Ut9nU8 (This will give you a full tour on how to run it, and how to use it)
-//When downloaded,  (if it is downloaded), and you watch the video on how to work it, go to token, and place the token key in the const token = '<token key' to run the bot
-//After that, hold these keys down at the same time for about 0.3 seconds, and do this "ctrl + `"
-//Then, type in next to PS C:(All info), type in "node .", and click enter for the bot to start. It should say "Your bot is now online, and running on all discord servers."
-//When you want to stop the bot, go to termianl, and hold these keys for about 0.3 seconds "ctrl + c" and the bot will stop running. To start, type in "node ." to start the bot again
-//If it isnt, read the errors below, and fix it. If you are having trouble, go back to my github, and look at the instructions there.
-//Thanks for downloading the bot! I hope you enjoy :)
+/*
+Welcome to the SuperBot! Code! This file here is the index.js file. This file runs all the other files in the Folder: BotCodeFiles.
+This file will contain all the information you need to help run it. Please watch the video or follow the instructions on GitHub to run
+this correctly. 
+
+Going to the other files in the Folder: BotCodeFiles, you will see all the code for every single command. I updated all the text on each
+file, and went in detail to explain what each section does for the command. I tried my best to explain how it works
+To see what command is in each file, go to BotCodeFiles on GitHub, and if you want to locate a command, 
+read the information below, and see what file that command is in. 
+
+Thanks for looking at my code for the bot! Please give feedback if you want to suggest or change anything. Thanks!
+*/
 
 const Discord = require('discord.js'); //without discord.js, the bot really cannot run -__-
-const doenvv = require('dotenv')
-const bot = new Discord.Client();
-const token = ("Place bot Token Here") //This token is important, and this is what runs the bot properly. Without it, the bot will not run.
-const PREFIX = ('b/' || 'sb!') //This prefix is b/. This is what the bot commands will respond to, when that prefix is used. Not even a joke, this bot can respond to this <letter><letter>. It is not even a joke
-var version = '1.2.0 (Stage Alpha)'; //This is the version of the bot. This is on top so I can change it anytime, without getting lost, and keep scrolling down, and down, and down...
-var help = 'This is being added soon...' //This is very unessessary
+const doenvv = require('dotenv') //Trying to make a .env file for this, but it has not worked in previous attempts for the TOKEN.
+const bot = new Discord.Client(); //Each command starts with "bot.on" or something else. Some people call this "client" for the const instead of bot.
+const token = ("Place Bot Token Here") //This token is important, and this is what runs the bot on Discord.
+const PREFIX = ('b/' || 'sb!') //The prefix of the bot. I tried adding new prefixes, but it doesn't work. 
+var version = '1.3.2 (Stage Alpha)'; //This is the version of the bot. This is on top so I can change it anytime, without getting lost, and keep scrolling down, and down, and down...
+var help = 'This is being added soon...' //Useless. Removing from code in 1.4.0. What is this anyway
 
 
 //All files here where the code is also is at is being ran at this file too. This is super important here. If not here, the bot,
@@ -26,10 +27,9 @@ const BasicCommands = require('./Bot Code Files/basiccommands'); //File basiccom
 const ModerationCommands = require('./Bot Code Files/moderationcommands'); //File moderationcommands.js is where all moderation commands are stored at
 const EctStuff = require('./Bot Code Files/ectstuff') //All other commands are being ran here.
 
-
 bot.on('ready', () =>{
-    console.log('The bot is successfully online!'); //When running the bot, after type and entering "node ." this message will appear below, saying the bot is online! There are no errors at all. The bot is 100% online
-    bot.user.setActivity('in ' + bot.guilds.cache.size + ' discord servers. b/help for commands. Version 1.2.0 Stage Alpha. Join our discord server: https://discord.gg/45Fkt7V') //This sets a custom status on the bot. 
+    console.log('The bot is successfully online!'); //When running the bot, in the terminal (or powershell), I will see this message indicating this file is running, and the status is running properly
+    bot.user.setActivity('VERSION 1.3.0!!!! Bot is on ' + bot.guilds.cache.size + ' discord servers. Join our discord server: https://discord.gg/45Fkt7V') //This sets a custom status on the bot. 
 })
  
 
